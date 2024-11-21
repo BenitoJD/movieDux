4d3b6117d9a304faa7002a4ac4b0de2ea2b07603
Benito JD <benito.josede@gmail.com>
2024-11-21 17:47:10 +0530

    added state

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -6,13 +6,24 @@ export default function MoviesGrid(){
 
 
     useEffect(() => {
-        const m = ["a","b","c"] 
-        setMovies(m);
+        fetch("movies.json")
+        .then(Response => Response.json())
+        .then(data=> setMovies(data))
     },[])
 
     return(
-    <div>
-{movies.length}
-    </div>
+    <div className='movies-grid'>
+    {
+        movies.map(movie => (
+            <div key={movie.id} className='movie-card'>
+                <img src={`images/${movie.image}`} alt={movie.title}/>
+                <div className='movie-card-title'>
+                  <h3 className='mocie-card-genre'>{movie.title}</h3>
+                  <p className='movie-card-genre'>{movie.genre}</p>
+                  <p className='movie-card-rating'>{movie.rating}</p>  
+                </div>
+            </div>
+        ))
+    }</div>
     );
 }
